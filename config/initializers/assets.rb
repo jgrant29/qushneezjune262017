3ec75ab4331558b996bbbdb49f386777c2e37a42
Justin Grant <jmgrant702@gmail.com>
2017-05-31 10:07:21 -0700

    image size reduction

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,26 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( kid_1.jpeg )
+Rails.application.config.assets.precompile += %w( kid_2.png )
+Rails.application.config.assets.precompile += %w( kid_3.jpeg )
+Rails.application.config.assets.precompile += %w( kid_4.jpeg )
+Rails.application.config.assets.precompile += %w( kid_5.JPG )
+Rails.application.config.assets.precompile += %w( kid_6.JPG )
+Rails.application.config.assets.precompile += %w( kid_7.jpg )
+Rails.application.config.assets.precompile += %w( kid_8.jpg )
+Rails.application.config.assets.precompile += %w( kid_9.jpg )
+Rails.application.config.assets.precompile += %w( our_story_pic.png )
+Rails.application.config.assets.precompile += %w( pant_feature.png )
+Rails.application.config.assets.precompile += %w( pant_feature_brown.png )
+Rails.application.config.assets.precompile += %w( qushneez_top.png )
+Rails.application.config.assets.precompile += %w( qushneez_Final.eps )
+Rails.application.config.assets.precompile += %w( qushneez_Final.svg )
+Rails.application.config.assets.precompile += %w( star.jpg )
+Rails.application.config.assets.precompile += %w( West4.jpg )
+
+
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
